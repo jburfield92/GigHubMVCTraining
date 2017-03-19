8d7c45a42e3fdd32f8fcb91abdc3c6792f225a9e
jburfield92 <jburfield@knights.ucf.edu>
2017-03-19 14:18:30 -0400

    added DTOs

---
+++ GigHub/Controllers/API/NotificationsController.cs
@@ -0,0 +1,53 @@
+﻿using GigHub.DTOs;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.API
+{
+    public class NotificationsController : ApiController
+    {
+        private ApplicationDbContext Context;
+
+        public NotificationsController()
+        {
+            Context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<NotificationDto> GetNewNotifications()
+        {
+            string userId = User.Identity.GetUserId();
+
+            // don't return domain objects to client via web APIs, use DTOs instead to modify the data returned
+            List<Notification> notifications = Context.UserNotifications
+                .Where(un => un.UserId == userId && !un.IsRead)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+            return notifications.Select(n => new NotificationDto()
+            {
+                DateTime = n.DateTime,
+                Gig = new GigDto
+                {
+                    Artist = new UserDto
+                    {
+                        Id = n.Gig.Artist.Id,
+                        Name = n.Gig.Artist.Name
+                    },
+                    DateAdded = n.Gig.DateAdded,
+                    Id = n.Gig.Id,
+                    IsCanceled = n.Gig.IsCanceled,
+                    Venue = n.Gig.Venue
+                },
+                OriginalDateTime = n.OriginalDateTime,
+                OriginalVenue = n.OriginalVenue,
+                Type = n.Type
+            });
+
+        }
+    }
+}
---
+++ GigHub/DTOs/GigDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GigHub.DTOs
+{
+    public class GigDto
+    {
+        public int Id { get; set; } // used for link to find the gig
+        public bool IsCanceled { get; set; }
+        public UserDto Artist { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Venue { get; set; }
+        public GenreDto Genre { get; set; }
+    }
+}
